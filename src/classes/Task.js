222e27b19a2f0d984213f8e68fa32bb0a293f46e
Oscar <ragnar@deulos.com>
2018-10-11 17:54:46 +0200

    Splited validation rules

--- src/classes/Task.js
+++ src/classes/Task.js
@@ -35,53 +35,66 @@ export default class Task {
 	}
 
 	checkValidation(validation) {
-		if (this.revalidate === false && validation.$result.$validated === true) {
+		if (this.revalidate === false && validation.$validated === true) {
 			if (validation.$result.hasError())
 				this.valid = false;
 
 			this.removeValidation(validation);
-
-			this.checkValidationsFinished();
 
 			return;
 		}
 
 		validation.$result.reset();
 
-		let validator, valid, ruleValue;
-
-		validation.$getRules().forEach((ruleName) => {
-			if (validation.$result.$validated)
+		validation.$getRules().forEach((rule) => {
+			if (validation.$validated)
 				return;
 
-			valid = false;
-			ruleValue = validation.$rules[ruleName];
-			validator = validation.$vm.$options.vdValidators[ruleName];
-
-			if (validator !== undefined) {
-				valid = validator.call(validation.$vm, ruleValue, validation.$data);
-
-			} else if (Utils.isFunction(ruleValue)) {
-				validator = validation.$rules[ruleName];
-				valid = validator.call(validation.$vm, validation.$data);
-
-			} else {
-				return;
-			}
-
-			if (Utils.isPromise(valid) === true) {
-				valid.then(() => {
-					this.addPathResult(validation, ruleName, true);
-
-				}).catch((error) => {
-					this.addPathResult(validation, ruleName, error.statusText || error);
-				});
-
-				return;
-			}
-
-			this.addValidationResult(validation, ruleName, valid);
+			this.checkValidationRule(validation, rule);
 		});
+	}
+
+	checkValidationRule(validation, ruleName) {
+		let valid = false;
+		let ruleValue = validation.$rules[ruleName];
+		let validator = validation.$vm.$options.vdValidators[ruleName];
+
+		if (validator !== undefined) {
+			if (Utils.isFunction(ruleValue))
+				ruleValue = ruleValue.call(validation.$vm);
+
+			valid = validator.call(validation.$vm, ruleValue, validation.$data);
+
+		} else if (Utils.isFunction(ruleValue)) {
+			validator = validation.$rules[ruleName];
+			valid = validator.call(validation.$vm, validation.$data);
+
+		} else {
+			return;
+		}
+
+		if (Utils.isPromise(valid) === true) {
+			valid.then(() => {
+				this.addValidationRuleResult(validation, ruleName, true);
+
+			}).catch((error) => {
+				this.addValidationRuleResult(validation, ruleName, error.statusText || error);
+			});
+
+			return;
+		}
+
+		this.addValidationRuleResult(validation, ruleName, valid);
+	}
+
+	addValidationRuleResult(validation, rule, valid) {
+		validation.$result.add(rule, valid);
+
+		if (valid !== true)
+			this.valid = false;
+
+		if (validation.$validated)
+			this.removeValidation(validation);
 	}
 
 	removeValidation(validation) {
@@ -89,18 +102,8 @@ export default class Task {
 
 		if (index !== -1)
 			this.validations.splice(index, 1);
-	}
 
-	addValidationResult(validation, rule, valid) {
-		validation.$result.add(rule, valid);
-
-		if (valid !== true)
-			this.valid = false;
-
-		if (validation.$result.$validated) {
-			this.removeValidation(validation);
-			this.checkValidationsFinished();
-		}
+		this.checkValidationsFinished();
 	}
 
 	checkValidationsFinished() {
