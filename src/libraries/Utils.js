b2db139043131089574d7cf96ca87edcd65b1f38
Oscar <ragnar@deulos.com>
2018-11-02 14:17:49 +0100

    Added pathToValue function

--- src/libraries/Utils.js
+++ src/libraries/Utils.js
@@ -23,3 +23,21 @@ export function isFunction(o) {
 export function extend(o, ...e) {
 	return Object.assign(o, ...e);
 }
+
+export function pathToValue(path, data) {
+	if (isString(path))
+		path = path.split('.');
+
+	let value;
+
+	try {
+		value = path.reduce((prev, cur) => {
+			return prev[cur];
+		}, data);
+
+	} catch(e) {
+		console.warn('Not a valid path for '+ path);
+	}
+
+	return value;
+}
